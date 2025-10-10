d6b6d8c01e0d3ca82c8af7df0af114d35258cb2b
nacho <castilloignacio1510@gmail.com>
2025-10-10 08:41:33 -0300

    add tooltip to the main title on the landing page

---
+++ src/components/pages/landing/PigeToolTip.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export default function PigeToolTip() {
+  return (
+    <>
+      <Tooltip>
+        <TooltipTrigger>
+          <span class="bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">
+            PIGE!
+          </span>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Plataforma Integral de Gestión Escolar</p>
+        </TooltipContent>
+      </Tooltip>
+    </>
+  );
+}
