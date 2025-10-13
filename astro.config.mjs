b5615b237d34c5c1a3155f29b4751dd500b6271b
nacho <castilloignacio1510@gmail.com>
2025-10-13 11:36:36 -0300

    improve astro astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import path from 'path';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+import path from "path";
 
 export default defineConfig({
   adapter: vercel(),
+  integrations: [react()],
+
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve('./src'), // <-- necesario
+        "@": path.resolve("./src"), // <-- necesario
       },
     },
   },
