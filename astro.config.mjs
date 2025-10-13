552922b6c063c8ac1924e5d1b7ee03f8454a0d87
nacho <castilloignacio1510@gmail.com>
2025-10-13 14:26:51 -0300

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
+import tailwindcss from "@tailwindcss/vite";
+import react from '@astrojs/react';
 import path from 'path';
 
 export default defineConfig({
   adapter: vercel(),
+    integrations: [react()],
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve('./src'), // <-- necesario
