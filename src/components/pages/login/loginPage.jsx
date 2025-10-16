279bcd8c8e6b56df386f9ac93147a11f83eaa5bf
nacho <castilloignacio1510@gmail.com>
2025-10-16 08:41:17 -0300

    feat: agregar estilos personalizados para la barra de desplazamiento en el layout

--- src/components/pages/login/loginPage.jsx
+++ src/components/pages/login/loginPage.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import CardPerson from "./CardPerson";
 import { Users } from "lucide-react";
 import Modal from "@/components/pages/login/Modal";
+import { Input } from "@/components/ui/Input";
+import { Label } from "@radix-ui/react-label";
 
 export default function LoginPage() {
   // Estado del modal y del estudiante seleccionado
@@ -14,7 +16,7 @@ export default function LoginPage() {
 
   // Recuperar datos
   useEffect(() => {
-    console.log("get students")
+    console.log("get students");
     fetch("/api/getStudents")
       .then((response) => response.json())
       .then((data) => setStudents(data));
@@ -35,25 +37,29 @@ export default function LoginPage() {
         student={selectedStudent}
       />
 
-      <main className="px-3 xl:p-0 overflow-auto scrollbar-hide">
+      <main className="px-3 xl:p-0 ">
         <h1 className="font-semibold text-center text-4xl">Iniciar sesión</h1>
         <p className="text-md text-center text-pretty">
           Debes buscar en la base de datos ya sea con tu nombre, apellido o DNI,
           y usar tu contraseña asignada.
         </p>
-
         {/* Input para buscar personas */}
-        <div className="flex flex-row items-center gap-2 border border-[var(--border)] w-fit rounded-md overflow-hidden group hover:border-blue-500 transition-colors mx-auto mt-12">
+        <div className="items-center gap-2  w-fit rounded-md  transition-colors mx-auto mt-12">
+          <Label htmlFor="email">Email</Label>
+          <Input type="email" id="email" placeholder="Email" />
+        </div>
+
+        <div className="flex flex-row items-center gap-2 border border-[var(--border)] w-fit rounded-md group hover:border-blue-500 transition-colors mx-auto mt-12">
           <div className="px-2 border-r border-[var(--border)] text-[var(--text-muted)]">
             <Users />
           </div>
+
           <input
             onChange={(e) => setFilter(e.target.value)}
             className="px-2 py-1 outline-none bg-transparent text-[var(--text)] placeholder-[var(--text-muted)]"
             placeholder="Buscar persona"
           />
         </div>
-
         {/* Lista de estudiantes */}
         <section className="flex flex-col gap-3 mx-auto w-full sm:w-2/3 lg:w-1/3 p-2 mt-14">
           {filtered.length === 0 ? (
